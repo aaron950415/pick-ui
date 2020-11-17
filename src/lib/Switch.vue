9d54ed40012706a667a86ff25185031dcea1bb90
Aaron <aaron950415@qq.com>
2020-11-17 16:02:55 +1100

    modify

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,44 +1,67 @@
 <template>
-  <button @click="toggle" :class="{ checked: checked }"><span></span></button>
+  <button
+    class="pick-switch"
+    @click="toggle"
+    :class="{'pick-checked': value }"
+  >
+    <span></span>
+  </button>
 </template>
 
 <script lang="ts">
 import { ref } from "vue";
 export default {
-  setup() {
-    const checked = ref(false);
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
     const toggle = () => {
-      checked.value = !checked.value;
+      context.emit("update:value", !props.value);
     };
-    return { checked, toggle };
+    return { toggle };
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
+.pick-switch {
   height: $h;
   width: $h * 2;
   border: none;
   background: grey;
   border-radius: $h/2;
   position: relative;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-}
-button.checked {
-  background: blue;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: left 250ms;
+  }
+  &.pick-checked {
+    background: blue;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.pick-checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
+  &:focus {
+    outline: none;
+  }
 }
 </style>
