c1d6868682c88ff95e3637651a4e344d9066278d
Aaron <aaron950415@qq.com>
2020-11-29 00:06:24 +1100

    完成Tab组件

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="pick-tabs">
+    <div class="pick-tabs-nav" ref="container">
+      <div
+        class="pick-tabs-nav-item"
+        :class="{ selected: t === selected }"
+        v-for="(t, index) in titles"
+        @click="select(t)"
+        :key="index"
+        :ref="
+          (el) => {
+            if (t === selected) selectedItem = el;
+          }
+        "
+      >
+        {{ t }}
+      </div>
+      <div class="pick-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="pick-tabs-content">
+      <component :is="current" :key="current.props.title"></component>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, onMounted, onUpdated, ref } from "vue";
+import Tab from "../lib/Tab.vue";
+export default {
+  components: { Tab },
+  props: {
+    selected: { type: String },
+  },
+  setup(props, context) {
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+
+    onMounted(() => {
+      const { width } = selectedItem.value.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+    });
+    onUpdated(() => {
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    });
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs child tag must be Tab");
+      }
+    });
+    const current = computed(() => {
+      return defaults.filter((tag) => tag.props.title === props.selected)[0];
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const select = (title: String) => {
+      context.emit("update:selected", title);
+    };
+    return {
+      defaults,
+      titles,
+      select,
+      selectedItem,
+      indicator,
+      container,
+      current,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.pick-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
